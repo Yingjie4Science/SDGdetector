5245dc58c47e2b2f4ff89fdf1165c0c0b82e9889
YingjieLiScience <yingjieli.edu@gmail.com>
2021-12-09 11:55:47 -0500

    Meng's earlier copy

--- R/findSDGs.R
+++ R/findSDGs.R
@@ -1,38 +1,79 @@
-# load database
-load('./R/SDG_keys.RData')
-library(dplyr)
+#' Identify SDGs in text
+#'
+#' @description
+#' The `findSDGs` function identify 17 Sustainable Development Goals and
+#' associated targets in text.
+#'
+#' @details
+#' In 2015, leaders worldwide adopted 17 Sustainable Development Goals (SDGs) with 169
+#' targets to be achieved by 2030 (https://sdgs.un.org). The framework of SDGs serves
+#' as a blueprint for shared prosperity for both people and the earth. `findSDGs`
+#' identifies both direct and indirect expressions of SDGs and associated targets in
+#' chunks of text. It takes a data frame with a specified column of text to process as
+#' inputs and outputs a data frame with original columns plus matched SDGs and targets.
+#'
+#' @name findSDGs
+#'
+#' @param df Data frame
+#' @param col Column name for text to be assessed
+#'
+#' @return
+#' Data frame with the same information as `df` and 18 extra columns: 17 columns marking
+#' the number of occurrence of each of the 17 SDGs and one column `match_detail` listing
+#' details of specific targets.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr mutate select relocate last_col
+#' @importFrom stringr str_count
+#'
+#' @export
+#'
+#' @examples
+#' my_text <- data.frame(my_col=c('our goal is to end poverty globally', 'this product
+#' contribute to slowing down climate change'))
+#' findSDGs(my_text, my_col)
 
-# identify 17 Sustainable Development Goals and associated targets in text
-# input: a dataframe with a column of "chr" type data
-# output: coded dataframe with columns: statement, matched_sdg, matched_target
 
-# separate goals/targets by column (then counts)?
+# load search terms of SDG targets
+load('data/SDG_keys.RData')
 
-findSDGs <- function(df) {
+findSDGs <- function(df, col) {
 
-  text_df <- df %>% dplyr::select(where(is.character)) # locate the column of text
-  colnames(text_df) <- "statement"
-  coded_df <- text_df %>% dplyr::mutate(matched_sdg = '', matched_target = '')
-                                        #sdg_count = '', target_count = '' # to add later
+  # initialize a column to record matched targets
+  coded_df <- df %>% mutate(match_detail='')
 
+  # loop over all patterns in database and record matches
   for (i in 1:nrow(SDG_keys)) {
-    sdg_id <- SDG_keys$SDG_id[i]
-    target_id <- SDG_keys$target_id[i]
-    target_key <- SDG_keys$SDG_keywords[i]
+    key <- SDG_keys$SDG_keywords[i]
+    id <- SDG_keys$SDG_id[i]
 
     coded_df <- coded_df %>%
-      dplyr::mutate(match = ifelse(grepl(pattern = target_key, x = statement, ignore.case = T, perl = T), 1, 0)) %>%
-      dplyr::mutate(matched_sdg = ifelse(match > 0, paste0(matched_sdg, sdg_id, ', '), matched_sdg),
-                    matched_target = ifelse(match > 0, paste0(matched_target, target_id, ', '), matched_target)) %>%
+      mutate(match = ifelse(grepl(pattern = key, x = {{col}}, ignore.case = T, perl = T), 1, 0)) %>%
+      mutate(match_detail = ifelse(match > 0, paste0(match_detail, id, ', '), match_detail)) %>%
       as.data.frame()
   }
-  coded_df <- subset(coded_df, select = -match)
+
+  # record the number of occurrence of each of the 17 SDGs according to matched targets
+  coded_df$SDG1 <- str_count(coded_df$match_detail, pattern = "SDG1_")
+  coded_df$SDG2 <- str_count(coded_df$match_detail, pattern = "SDG2_")
+  coded_df$SDG3 <- str_count(coded_df$match_detail, pattern = "SDG3_")
+  coded_df$SDG4 <- str_count(coded_df$match_detail, pattern = "SDG4_")
+  coded_df$SDG5 <- str_count(coded_df$match_detail, pattern = "SDG5_")
+  coded_df$SDG6 <- str_count(coded_df$match_detail, pattern = "SDG6_")
+  coded_df$SDG7 <- str_count(coded_df$match_detail, pattern = "SDG7_")
+  coded_df$SDG8 <- str_count(coded_df$match_detail, pattern = "SDG8_")
+  coded_df$SDG9 <- str_count(coded_df$match_detail, pattern = "SDG9_")
+  coded_df$SDG10 <- str_count(coded_df$match_detail, pattern = "SDG10_")
+  coded_df$SDG11 <- str_count(coded_df$match_detail, pattern = "SDG11_")
+  coded_df$SDG12 <- str_count(coded_df$match_detail, pattern = "SDG12_")
+  coded_df$SDG13 <- str_count(coded_df$match_detail, pattern = "SDG13_")
+  coded_df$SDG14 <- str_count(coded_df$match_detail, pattern = "SDG14_")
+  coded_df$SDG15 <- str_count(coded_df$match_detail, pattern = "SDG15_")
+  coded_df$SDG16 <- str_count(coded_df$match_detail, pattern = "SDG16_")
+  coded_df$SDG17 <- str_count(coded_df$match_detail, pattern = "SDG17_")
+
+  # organize data frame for output
+  coded_df <- coded_df %>% select(-match) %>% relocate(match_detail, .after=last_col())
+
   return(coded_df)
 }
-
-test <- data.frame(boo=c('To this end, we tailor our products and services to suit our customers specific needsfrom home construction, improvement, and renovation to agricultural, industrial, and marine/hydraulic applications',
-                         'define and carry out a worldwide survey across Working with a diverse group of stakeholderslistening to their concerns and managing our relations in a proactive and fruitful wayis crucial to understanding our ecosystem and maximizing our positive impact in the places where we operate',
-                         'We are further leading the UN Global compact in Mexico, encouraging more companies to partner to contribute and explore business opportunities while reducing negative impact, and creating shared value to society'))
-
-coded_test <- findSDGs(test)
-
