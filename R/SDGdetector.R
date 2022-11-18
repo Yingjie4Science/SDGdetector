2c1029838a2493f3cd61e427b1acf1591bcb4ad8
YingjieLiScience <yingjieli.edu@gmail.com>
2022-11-17 21:33:37 -0800

    test

--- R/SDGdetector.R
+++ R/SDGdetector.R
@@ -37,80 +37,81 @@
 # load search terms of SDG targets
 load('data/SDG_keys.RData')
 
-SDGdetector <- function(x, col = 1) {
-  
+SDGdetector <- function(x, col) {
+
   ## first, to check the input is a string or a dataframe --------------------------------
 
   if (is.data.frame(x) == FALSE) {
-    ## -> if not a dataframe 
+    ## -> if not a dataframe
     # print('change/put the string into a dataframe')
-    df <- data.frame(x = x)
-    
+    df <- data.frame(col = x) %>%
+      dplyr::mutate(id = dplyr::row_number())
+
   } else {
     ##
     df <- x
   }
-  
-  
+
+
   ## -------------------------------------------------------------------------------------
-  code <- df %>% 
-    dplyr::mutate(#match   = 0,      
+  code <- df %>%
+    dplyr::mutate(#match   = 0,
       sdgs    = '', ## for later use, to append data to this column
       n_total = 0,  ## count the accumulated mentions
       sdgs_n  = '') ## ## combine together SDG names and the number of mentions
-  
+
   for (i in 1:nrow(SDG_keys)){                           ## loop each SDG indicators
     sdg_i_str <- SDG_keys$SDG_id[i] %>% as.character()   ## get the SDG id name
     sdg_i_obj <- SDG_keys$SDG_keywords[i]                ## get the corresponding SDG search term list
-    
+
     print(sdg_i_str)
     # print(sdg_i_obj)
-    
+
     code <-  code %>%
       as.data.frame() %>%
-      
+
       ## at the sentence level - count once if goals/targets are mentioned ---------------
       dplyr::mutate(
         match = ifelse(
-          grepl(pattern = sdg_i_obj, x = col, ignore.case = T, perl = T), 1, 0))  %>% ## yes-1 or no-0 if they match
-        dplyr::mutate(
-          sdgs  = ifelse(match > 0, paste0(sdgs, ',', sdg_i_str), sdgs)) %>%
-        dplyr::select(-match) %>% ## remove this column 
-        
-        ## at the sentence level - count the times of all the mentions -------------------
+          grepl(pattern = sdg_i_obj, x = as.character({{col}}), ignore.case = T, perl = T), 1, 0))  %>% ## yes-1 or no-0 if they match
       dplyr::mutate(
-        n       = str_count(string = col, regex(pattern = sdg_i_obj, ignore_case = T)),
-        n_total = n_total + n,
-        sdgs_n  = ifelse(n > 0, paste0(sdgs_n, ',', sdg_i_str, '-', n), sdgs_n)
-      ) %>%
-        dplyr::select(-n) %>%   ## remove this column 
-        as.data.frame()
+          sdgs  = ifelse(match > 0, paste0(sdgs, ',', sdg_i_str), sdgs)) %>%
+      dplyr::select(-match) %>% ## remove this column
+
+      ## at the sentence level - count the times of all the mentions -------------------
+      # dplyr::mutate(
+      #   n       = str_count(string = as.character({{col}}), regex(pattern = sdg_i_obj, ignore_case = T)),
+      #   n_total = n_total + n,
+      #   sdgs_n  = ifelse(n > 0, paste0(sdgs_n, ',', sdg_i_str, '-', n), sdgs_n)
+      # ) %>%
+      # dplyr::select(-n) %>%   ## remove this column
+      as.data.frame()
   }
-  
-  
+
+
   ### sort from most SDG hits to least (or, none)
-  coded <- code %>% arrange(desc(nchar(sdgs)), id)
-  
+  coded <- code %>% dplyr::arrange(desc(nchar(sdgs)), id)
+
   return(coded)
-  
-  
+
+
   ### by Meng Cai ------------------------------------------------------------------------
   # # initialize a column to record matched targets
-  # match_detail <- ''
-  # coded_df <- df %>% dplyr::mutate(match_detail=match_detail)
-  # 
-  # # loop over all patterns in database and record matches
-  # for (i in 1:nrow(SDG_keys)) {
-  # 
-  #   key <- SDG_keys$SDG_keywords[i]
-  #   id <- SDG_keys$SDG_id[i]
-  # 
-  #   coded_df <- coded_df %>%
-  #     dplyr::mutate(match = ifelse(grepl(pattern = key, x = as.character({{col}}), ignore.case = T, perl = T), 1, 0)) %>%
-  #     dplyr::mutate(match_detail = ifelse(match > 0, paste0(match_detail, id, ', '), match_detail)) %>%
-  #     as.data.frame()
-  # }
-  # 
+  match_detail <- ''
+  coded_df <- df %>% dplyr::mutate(match_detail=match_detail)
+
+  # loop over all patterns in database and record matches
+  for (i in 1:nrow(SDG_keys)) {
+
+    key <- SDG_keys$SDG_keywords[i]
+    id <- SDG_keys$SDG_id[i]
+
+    coded_df <- coded_df %>%
+      dplyr::mutate(match = ifelse(grepl(pattern = key, x = as.character({{col}}), ignore.case = T, perl = T), 1, 0)) %>%
+      dplyr::mutate(match_detail = ifelse(match > 0, paste0(match_detail, id, ', '), match_detail)) %>%
+      as.data.frame()
+  }
+  #
   # # record the number of occurrence of each of the 17 SDGs according to matched targets
   # coded_df$SDG1 <- str_count(coded_df$match_detail, pattern = "SDG1_")
   # coded_df$SDG2 <- str_count(coded_df$match_detail, pattern = "SDG2_")
@@ -129,10 +130,10 @@ SDGdetector <- function(x, col = 1) {
   # coded_df$SDG15 <- str_count(coded_df$match_detail, pattern = "SDG15_")
   # coded_df$SDG16 <- str_count(coded_df$match_detail, pattern = "SDG16_")
   # coded_df$SDG17 <- str_count(coded_df$match_detail, pattern = "SDG17_")
-  # 
+  #
   # # organize data frame for output
   # coded_df <- coded_df %>% dplyr::select(-match) %>% relocate(match_detail, .after=last_col())
-  # 
+  #
   # return(coded_df)
 
 }
