899e9d21735822c43091734e083e1f97bb613ca7
YingjieLiScience <yingjieli.edu@gmail.com>
2022-11-23 16:19:49 -0800

    add test examples

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,25 @@
-devtools::install_github("Yingjie4Science/SDGdetector")
-library(testthat)
-library(SDGdetector)
+# if (!require("remotes")) {
+#   install.packages("remotes")
+# }
+#
+# remotes::install_github("Yingjie4Science/SDGdetector")
+# library(SDGdetector)
+# citation(package = 'SDGdetector')
 
+library(testthat)
 test_check("SDGdetector")
+??SDGdetector
+
+
+## string as input data
+x <- 'our goal is to mitigate climate change, end poverty, and reducing inequality globally'
+SDGdetector(x)
+
+
+## dataframe as input data
+my_text <- data.frame(my_col=c('our goal is to end poverty globally',
+                               'this product contributes to slowing down climate change',
+                               'ensure access to updated information'))
+SDGdetector(x = my_text, col = my_col)
+
+
