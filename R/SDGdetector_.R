a2389b8f24a2e04bef1a18819bec4ce0dc2e9655
YingjieLiScience <yingjieli.edu@gmail.com>
2022-11-26 14:36:35 -0800

    add the main file for the package?

---
+++ R/SDGdetector_.R
@@ -0,0 +1,32 @@
+#' Detecting UN Sustainable Development Goals in Text
+#'
+#' The `SDGdetector` function identify 17 Sustainable Development Goals and
+#' associated 169 targets in text.
+#' The following provides a brief overview of the contents of the package.
+#'
+#' @section Detect functions:
+#'
+#'   \code{\link{SDGdetector}} detects SDGs in text
+#'   \code{\link{detect_region}} detect country or region names in text for further mapping
+#'
+#' @section Analysis functions:
+#'
+#'   \code{\link{plot_sdg_bar}} visualizes the relative frequency of SDG in barplots
+#'
+#'   \code{\link{plot_sdg_map}}
+#'
+#' @section Datasets:
+#'
+#'   \code{\link{SDG_keys}}
+#'
+#'
+#' @examples
+#' \donttest{
+#' # detect SDGs
+#' SDGdetector(x)
+#'
+#'
+#' @docType package
+#' @name SDGdetector
+NULL
+#> NULL
