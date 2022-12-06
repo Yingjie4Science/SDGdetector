e9fd9feea52b1d79ef6251fbff8f6656df3011c9
YingjieLiScience <yingjieli.edu@gmail.com>
2022-12-05 21:44:10 -0800

    test on building the package

--- R/SDGdetector_package.R
+++ R/SDGdetector_package.R
@@ -28,5 +28,10 @@
 #'
 #' @docType package
 #' @name SDGdetector
+#'
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
 #> NULL
