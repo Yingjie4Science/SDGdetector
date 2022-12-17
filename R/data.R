329041405e2a765c9e6c3fbbbd902de55a98c788
YingjieLiScience <yingjieli.edu@gmail.com>
2022-12-17 15:51:25 -0800

    added by Yongze

---
+++ R/data.R
@@ -0,0 +1,56 @@
+
+
+#' @title Datasets of country and region names.
+#'
+#' @description Datasets of country and region names.
+#'
+#' @name country_region_names
+#' @format \code{country_region_names}: A data frame with 644 rows and 3 variables
+#' @docType data
+#' @author Yingjie Li \email{yingjieli.edu@gmail.com}
+#' @keywords dataset
+"country_region_names"
+
+#' @title Datasets of shapefiles.
+#'
+#' @description Datasets of shapefiles..
+#'
+#' @name shp
+#' @format \code{shp}: A data frame with 241 rows and 6 variables
+#' @docType data
+#' @author Yingjie Li \email{yingjieli.edu@gmail.com}
+#' @keywords dataset
+"shp"
+
+
+#' @title Datasets of SDG statistics.
+#'
+#' @description Datasets of SDG statistics.
+#'
+#' @name sdgstat
+#' @format \code{sdgstat}: A data frame with 62 rows and 4 variables
+#' @docType data
+#' @author Yingjie Li \email{yingjieli.edu@gmail.com}
+#' @keywords dataset
+"sdgstat"
+
+
+
+
+#' @title Datasets of SDG keys.
+#'
+#' @description Datasets of SDG keys.
+#'
+#' @name SDG_keys
+#' @format \code{SDG_keys}: A data frame with 557 rows and 3 variables
+#' @docType data
+#' @author Yingjie Li \email{yingjieli.edu@gmail.com}
+#' @keywords dataset
+"SDG_keys"
+
+
+
+
+
+
+
---
+++ R/globals.R
@@ -0,0 +1,1 @@
+utils::globalVariables(c("SDG_keys", "country_region_names"))
